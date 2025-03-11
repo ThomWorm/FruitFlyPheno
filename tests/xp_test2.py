ace7798970187b0ae469c2c6c56f9a1ea7d79223
TWorm <tworm@ncsu.edu>
2025-03-10 23:21:36 -0400

    plot and process caching

--- tests/xp_test2.py
+++ tests/xp_test2.py
@@ -3,12 +3,24 @@ import xpublish
 import numpy as np
 import pandas as pd
 
-output_zarr_store = r"simple_zarr_dataset.zarr"  # Replace with the desired output path
+data = xr.Dataset(
+    {
+        "temperature": (("time", "lat", "lon"), 20 * np.random.rand(4, 3, 2)),
+    },
+    coords={
+        "time": pd.date_range("2023-01-01", periods=4),
+        "lat": [10, 20, 30],
+        "lon": [100, 110],
+    },
+)
+print(type(data))
 
-data = xr.open_zarr(output_zarr_store)
-
-
-rest = xpublish.SingleDatasetRest(data)
+d2 = xr.open_zarr(
+    r"/home/thom/Desktop/CIPM/FruitFlyPheno/tests/simple_zarr_dataset.zarr",
+    consolidated=True,
+)
+print(type(d2))
+rest = xpublish.SingleDatasetRest(d2)
 # Start the server and keep it running
 if __name__ == "__main__":
     print("Serving simple dataset...")
